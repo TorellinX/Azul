7877d7a7c7f6d8e51b969fed2dfedfb65adf050e
Morozova Alena <morozovaalenay@gmail.com>
2022-06-27 23:03:16 +0200

    updated addColorTilesToLine(..), countFreeFieldsInRo(..), added a test addColorTilesToLine_WhenFits(), createPatternLines(), Initiation of the wall, equals() for ColorTile + check style

--- src/test/java/de/lmu/ifi/sosylab/model/ModelTest.java
+++ src/test/java/de/lmu/ifi/sosylab/model/ModelTest.java
@@ -1,9 +1,11 @@
 package de.lmu.ifi.sosylab.model;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -40,6 +42,27 @@ public class ModelTest {
     GameModel model = newModel(testPlayers);
     int amount = testPlayers.size() * 2 + 1;
     assertEquals(model.getPlates().size(), amount);
+  }
+
+  @Test
+  public void addColorTilesToLine_whenFits() {
+    GameModel model = newModel(testPlayers);
+    model.getPlayers().get(0).playerBoard.patternLines[4] = new ColorTile[]{null, null, null, null,
+        new ColorTile(Color.RED)};
+    ArrayList<ColorTile> redTiles = new ArrayList<>();
+    for (int i = 0; i < model.getPlayers().get(0).playerBoard.patternLines[4].length - 1; i++) {
+      redTiles.add(new ColorTile(Color.RED));
+    }
+
+    model.getPlayers().get(0).playerBoard.addColorTilesToLine(redTiles, 4);
+
+    ColorTile[] redTilesInLine = new ColorTile[model.getPlayers()
+        .get(0).playerBoard.patternLines[4].length];
+    for (int i = 0; i < redTilesInLine.length; i++) {
+      redTilesInLine[i] = new ColorTile(Color.RED);
+    }
+
+    assertArrayEquals(model.getPlayers().get(0).playerBoard.patternLines[4], redTilesInLine);
   }
 
 }

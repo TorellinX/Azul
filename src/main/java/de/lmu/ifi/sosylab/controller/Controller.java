3f31b9b5fc9975300e06e3316da61937334922c7
Dominik Weis <weisdo@cip.ifi.lmu.de>
2022-06-28 00:15:02 +0200

    added JavaDoc

--- src/main/java/de/lmu/ifi/sosylab/controller/Controller.java
+++ src/main/java/de/lmu/ifi/sosylab/controller/Controller.java
@@ -1,27 +1,2 @@
-package de.lmu.ifi.sosylab.controller;
-
-import static java.util.Objects.requireNonNull;
-
-import de.lmu.ifi.sosylab.model.Color;
-import de.lmu.ifi.sosylab.model.GameModel;
-import de.lmu.ifi.sosylab.model.Plate;
-import de.lmu.ifi.sosylab.model.Player;
-
-public class Controller {
-
-  GameModel model;
-
-  public Controller(GameModel model) {
-    this.model = requireNonNull(model);
-  }
-
-  public void placeTilesFromPlateEvent(Plate plate, Color color, Player player, int row) {
-    model.pickTilesFromPlate(plate,color,player, row);
-  }
-
-  public void placeTilesFromTableCenterEvent(Color color, Player player, int row) {
-    model.pickTilesFromTableCenter(color, player, row);
-  }
-
-
+package de.lmu.ifi.sosylab.controller;public interface Controller {
 }

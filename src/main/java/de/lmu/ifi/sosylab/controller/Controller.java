9e94f4f4d46fdbf787668dba6b106245e59b1787
Dominik Weis <weisdo@cip.ifi.lmu.de>
2022-06-27 23:09:08 +0200

    first versionf controller

--- src/main/java/de/lmu/ifi/sosylab/controller/Controller.java
+++ src/main/java/de/lmu/ifi/sosylab/controller/Controller.java
@@ -1,27 +1,27 @@
 package de.lmu.ifi.sosylab.controller;
 
+import static java.util.Objects.requireNonNull;
+
 import de.lmu.ifi.sosylab.model.Color;
+import de.lmu.ifi.sosylab.model.GameModel;
 import de.lmu.ifi.sosylab.model.Plate;
 import de.lmu.ifi.sosylab.model.Player;
 
-public interface Controller {
+public class Controller {
 
-  /**
-   * place Tiles selected from Plate on PlayerBoard
-   * @param plate Plate from where the Tiles are coming.
-   * @param color Color of the selected Tiles.
-   * @param player current Player.
-   * @param row selected Row.
-   */
-  void placeTilesFromPlateEvent(Plate plate, Color color, Player player, int row);
+  GameModel model;
 
-  /**
-   * place Tiles selected from TableCenter on PlayerBoard.
-   * @param color Color of the selected Tiles.
-   * @param player current Player.
-   * @param row selected Row.
-   */
-  void placeTilesFromTableCenterEvent(Color color, Player player, int row);
-
-
+  public Controller(GameModel model) {
+    this.model = requireNonNull(model);
   }
+
+  public void placeTilesFromPlateEvent(Plate plate, Color color, Player player, int row) {
+    model.pickTilesFromPlate(plate,color,player, row);
+  }
+
+  public void placeTilesFromTableCenterEvent(Color color, Player player, int row) {
+    model.pickTilesFromTableCenter(color, player, row);
+  }
+
+
+}

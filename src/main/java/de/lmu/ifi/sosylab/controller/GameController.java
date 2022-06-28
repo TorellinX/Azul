fe40177f07cf2ee5d149e791cb3ec67035917299
Dominik Weis <weisdo@cip.ifi.lmu.de>
2022-06-28 17:50:50 +0200

    added pick check

--- src/main/java/de/lmu/ifi/sosylab/controller/GameController.java
+++ src/main/java/de/lmu/ifi/sosylab/controller/GameController.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import de.lmu.ifi.sosylab.model.Color;
 import de.lmu.ifi.sosylab.model.GameModel;
 import de.lmu.ifi.sosylab.model.Plate;
+import de.lmu.ifi.sosylab.model.PlateState;
 import de.lmu.ifi.sosylab.model.Player;
 import de.lmu.ifi.sosylab.model.PlayerState;
 import java.util.ArrayList;
@@ -19,6 +20,23 @@ public class GameController implements Controller{
 
   public GameController(GameModel model) {
     this.model = requireNonNull(model);
+  }
+
+  public void pickTilesFromPlateCheck(Plate plate, Color color) {
+    if(!model.getPlates().stream().anyMatch(p -> p == plate)){
+      //TODO: This Plate does not exist.
+    }
+    if(plate.getState() == PlateState.EMPTY){
+      //TODO This Plate is empty.
+    }
+    if(!plate.containsColor(color)){
+      //TODO: This Color does not exist on this Plate.
+    }
+    //TODO: Notify valid MOVE
+  }
+
+  public void pickTilesFromTableCenter(Color color) {
+
   }
 
   public void placeTilesFromPlateEvent(Plate plate, Color color, Player player, int row) {

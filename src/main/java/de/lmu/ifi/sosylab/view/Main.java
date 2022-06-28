52837d6f10ec7a279cecd57818490a70592e5180
Lena <Lena.Hacker@campus.lmu.de>
2022-06-28 22:34:51 +0200

    Markus View läuft.

---
+++ src/main/java/de/lmu/ifi/sosylab/view/Main.java
@@ -0,0 +1,8 @@
+package de.lmu.ifi.sosylab.view;
+
+public class Main {
+
+  public static void main(String[] args){
+    main.java.de.lmu.ifi.sosylab.view.AzulView myView = new main.java.de.lmu.ifi.sosylab.view.AzulView();
+  }
+}

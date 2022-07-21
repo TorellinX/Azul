4a1ff30e097b3ac66b3d295daadc2c12eb6ff929
Dominik Weis <weisdo@cip.ifi.lmu.de>
2022-07-21 09:50:29 +0200

    basic Websocket Event Listener

---
+++ src/main/java/de/lmu/ifi/sosylab/server/controller/WebSocketEventListener.java
@@ -0,0 +1,30 @@
+package de.lmu.ifi.sosylab.server.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+@Component
+public class WebSocketEventListener {
+
+  private static final Logger log = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+  @Autowired
+  private SimpMessageSendingOperations sendingOperations;
+
+  @EventListener
+  public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+    log.info("New connection! " + event.getMessage());
+  }
+
+  @EventListener
+  public void handleWebSocketDisconnectListener(SessionConnectedEvent event) {
+
+    log.info("Disconnected! " + event);
+  }
+}
